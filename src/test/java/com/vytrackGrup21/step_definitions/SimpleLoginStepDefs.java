1354cbd1e41cf3e094bc06bddc119926f495e6aa
Mhkngithub <greeklenovo@outlook.com>
2020-11-23 16:40:30 +0200

    3

--- src/test/java/com/vytrackGrup21/step_definitions/SimpleLoginStepDefs.java
+++ src/test/java/com/vytrackGrup21/step_definitions/SimpleLoginStepDefs.java
@@ -30,5 +30,50 @@ public class SimpleLoginStepDefs {
     }
 
 
+    @When("Sales Manager login with valid credantial")
+    public void sales_Manager_login_with_valid_credantial() {
+
+        LoginPage loginPage = new LoginPage();
+        loginPage.userName.sendKeys(ConfigurationReader.get("sales_manager_username"));
+        BrowserUtils.waitFor(1);
+        loginPage.password.sendKeys(ConfigurationReader.get("sales_manager_password"));
+        loginPage.loginButton.click();
+    }
+
+    @Then("Sales Manager see Dashboard page")
+    public void sales_Manager_see_Dashboard_page() {
+        //Dashboard dashboard = new Dashboard();
+        System.out.println(Driver.get().getTitle());
+        String actual =Driver.get().getTitle();
+        Assert.assertEquals("Dashboard",actual);
+
+    }
+
+    @When("Store Manager login with valid credantial")
+    public void store_Manager_login_with_valid_credantial() {
+
+        LoginPage loginPage = new LoginPage();
+        loginPage.userName.sendKeys(ConfigurationReader.get("store_manager_username"));
+        BrowserUtils.waitFor(1);
+        loginPage.password.sendKeys(ConfigurationReader.get("store_manager_password"));
+        loginPage.loginButton.click();
+
+    }
+
+    @Then("Store Manager see Dashboard page")
+    public void store_Manager_see_Dashboard_page() {
+        //Dashboard dashboard = new Dashboard();
+        System.out.println(Driver.get().getTitle());
+        String actual =Driver.get().getTitle();
+        BrowserUtils.waitFor(1);
+        Assert.assertEquals("Dashboard",actual);
+    }
+
+
+
+
+
+
+
 
 }
